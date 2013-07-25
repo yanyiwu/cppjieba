b2775888d138215560c5da82b25941ad30ea2da2
gwdwyy <wuyanyi09@gmail.com>
2013-07-25 11:59:41 +0800

    modify demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -8,8 +8,13 @@ using namespace std;
 
 using namespace CppJieba;
 
-int main()
+int main(int argc, char ** argv)
 {
+	if(argc < 2)
+	{
+		cerr<<"usage: "<<argv[0]<<" filename"<<endl;
+		return 1;
+	}
 	KeyWordExt ext;
 	ext.init();
 	
