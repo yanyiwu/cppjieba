8d3759e22f2ea000a183074312d7f6e6d69f0b86
gwdwyy <wuyanyi09@gmail.com>
2013-07-27 15:15:03 +0800

    add headers.h

---
+++ src/headers.h
@@ -0,0 +1,15 @@
+/************************************
+ * file enc : ASCII
+ * author   : wuyanyi09@gmail.com
+************************************/
+#ifndef CPPJIEBA_HEADERS_H
+#define CPPJIEBA_HEADERS_H
+
+#include "cppcommon/headers.h"
+#include "globals.h"
+#include "KeyWordExt.h"
+#include "Segment.h"
+#include "Trie.h"
+#include "TransCode.h"
+
+#endif
