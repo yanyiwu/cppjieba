7c7d5e29bc69e0f3d77e78d92a12fda4e5d14c0c
aholic <ruochen.xu@gmail.com>
2013-12-16 06:03:04 +0800

    update Limonp, add TrieManager to manage tries

---
+++ src/TrieManager.hpp
@@ -0,0 +1,74 @@
+#ifndef CPPJIEBA_TRIEMANAGER_H
+#define CPPJIEBA_TRIEMANAGER_H
+
+#include "Trie.hpp"
+#include "Limonp/md5.hpp"
+#include "Limonp/logger.hpp"
+
+namespace CppJieba
+{
+    using namespace Limonp;
+    class TrieManager
+    {
+        private:
+            unordered_map<const char*, Trie*> _tries;
+            TrieManager(){};
+            TrieManager(TrieManager& tm){};
+        public:
+            Trie* getTrie(const char* dictpath) 
+            {
+                string md5;
+                if (!md5File(dictpath, md5))
+                {
+                    LogError("error when getting md5 for file '%s'", dictpath);
+                    return NULL;
+                }
+                else
+                {
+                    if (_tries.find(md5.c_str()) == _tries.end())
+                    {
+                        Trie* trie = NULL;
+                        try
+                        {
+                            trie = new Trie();
+                        }
+                        catch (const bad_alloc& e)
+                        {
+                            LogError("error when new a trie for file '%s'", dictpath);
+                            return NULL;
+                        }
+                        if (NULL == trie)
+                            return NULL;
+
+                        if (!trie->init())
+                        { 
+                            LogError("trie init error for file '%s'", dictpath);
+                            return NULL;
+                        }
+
+                        LogInfo("trie->loadDict(%s) start...", dictpath);
+                        if (!trie->loadDict(dictpath))
+                        {
+                            LogError("trie->loadDict(%s) failed...", dictpath);
+                            return NULL;
+                        }
+                        LogInfo("trie->loadDict end...");
+
+                        _tries[md5.c_str()] = trie;
+                        return trie;
+                    }
+                    else
+                    {
+                        return _tries[md5.c_str()];
+                    }
+                }
+            }
+
+            static TrieManager& getInstance()
+            {
+                static TrieManager _this;
+                return _this;
+            }
+    };
+}
+#endif
