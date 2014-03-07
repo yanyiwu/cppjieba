a7f4e1802731ff146489f544e39bca88a20c76f4
wyy <wuyanyi09@gmail.com>
2014-03-07 18:35:14 +0800

    ci TKeywordExtractor.cpp to fix bug which test in x64 and x86 not the same

--- test/unittest/TKeywordExtractor.cpp
+++ test/unittest/TKeywordExtractor.cpp
@@ -8,11 +8,11 @@ using namespace CppJieba;
 TEST(KeywordExtractorTest, Test1)
 {
     KeywordExtractor extractor("../dict/jieba.dict.utf8", "../dict/hmm_model.utf8", "../dict/idf.utf8");
-    string s("我是蓝翔技工拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上总经理，出任CEO，迎娶白富美，走上人生巅峰。");
+    string s("我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上总经理，迎娶白富美，走上人生巅峰。");
     string res;
     vector<pair<string, double> > wordweights;
     size_t topN = 5;
     extractor.extract(s, wordweights, topN);
     res << wordweights;
-    ASSERT_EQ(res, "[\"CEO:11.7392\", \"蓝翔:11.7392\", \"白富美:11.7392\", \"升职:10.8562\", \"加薪:10.6426\"]");
+    ASSERT_EQ(res, "[\"白富美:11.7392\", \"升职:10.8562\", \"加薪:10.6426\", \"迎娶:10.0505\", \"手扶拖拉机:10.0089\"]");
 }
