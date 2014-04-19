be3773920a3a7c38ed2e3cdc409f183f8674f0b7
wyy <wuyanyi09@gmail.com>
2014-04-20 00:23:42 +0800

    modify keyword_demo

--- test/keyword_demo.cpp
+++ test/keyword_demo.cpp
@@ -6,8 +6,11 @@ int main(int argc, char ** argv)
     KeywordExtractor extractor("../dict/jieba.dict.utf8", "../dict/hmm_model.utf8", "../dict/idf.utf8", "../dict/stop_words.utf8");
     string s("我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。");
     vector<pair<string, double> > wordweights;
+    vector<string> words;
     size_t topN = 5;
     extractor.extract(s, wordweights, topN);
     cout<< s << '\n' << wordweights << endl;
+    extractor.extract(s, words, topN);
+    cout<< s << '\n' << words << endl;
     return EXIT_SUCCESS;
 }
