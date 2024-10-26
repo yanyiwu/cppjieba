076d5e524bc49da1143655e5653b05e9f1a76672
yanyiwu <wuyanyi09@foxmail.com>
2024-10-26 12:14:56 +0800

    [encoding] MSC #pragma execution_character_set("utf-8")

--- include/cppjieba/Jieba.hpp
+++ include/cppjieba/Jieba.hpp
@@ -1,6 +1,10 @@
 #ifndef CPPJIEAB_JIEBA_H
 #define CPPJIEAB_JIEBA_H
 
+#if _MSC_VER >= 1600
+  #pragma execution_character_set("utf-8")
+#endif
+
 #include "QuerySegment.hpp"
 #include "KeywordExtractor.hpp"
 
